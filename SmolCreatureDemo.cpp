c819d9cfa8e75a08aee6a9b6060d7bb2b8510b1f
Doug Arley <darley@fullsail.com>
2025-06-25 15:15:27 -0400

    Moved events to Game class

--- SmolCreatureDemo.cpp
+++ SmolCreatureDemo.cpp
@@ -1,12 +1,10 @@
 // SmolCreatureDemo.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
-#include "Events.h"
+#include "Game.h"
 
 int main()
 {
-	// Call a series of Events
-	Handler player = Events::Intro();
-	Events::WakeUp(player.GetName());
-	Events::TouchGrass(player.GetName());
+	Game game;
+	game.Start();
 }
